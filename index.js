e6d8a4cd94cb9f4c3fa2ccd10656e9a2241a3e46
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-05-20 10:53:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function testFunc() {
 function superbowlWin(Array){
   let result;
   Array.forEach((item)=>{
-    if(item.result == "W") result = item.year;
+    if(item.result === "W") result = item.year;
   });
   return result;
 }
